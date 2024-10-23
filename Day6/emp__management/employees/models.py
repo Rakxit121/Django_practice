c89774d3904b6be86f1f2f01816e3e8a91fcee33
Rakxit121 <rakxit121@outlook.com>
2024-10-23 11:55:22 +0545

    django path part error

--- Day6/emp__management/employees/models.py
+++ Day6/emp__management/employees/models.py
@@ -8,6 +8,7 @@ class Employee(models.Model):
     email = models.EmailField(unique=True)
     status = models.IntegerField(choices=[(1, 'Active'), (2, 'Terminated')])
     qualification = models.CharField(max_length=255)
+    profile_image = models.ImageField(upload_to='profiles/', null=True, blank=True)
 
     def __str__(self):
         return self.name
