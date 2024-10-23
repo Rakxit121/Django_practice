196a2dad263136947c6ed4655b71875f57806b76
Rakxit121 <rakxit121@outlook.com>
2024-10-23 10:52:56 +0545

    day 6 flutter app start

---
+++ Day6/emp__management/employees/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    employee_id = models.CharField(max_length=50)
+    gender = models.IntegerField(choices=[(1, 'Male'), (2, 'Female')])
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    status = models.IntegerField(choices=[(1, 'Active'), (2, 'Terminated')])
+    qualification = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
